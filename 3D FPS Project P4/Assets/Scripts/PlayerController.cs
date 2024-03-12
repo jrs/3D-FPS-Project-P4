56fac1c99a6ab5a8ff6783d69c2e18ed0a6a823d
Justin Scott <hello@jrs.dev>
2024-03-12 12:55:06 -0700

    Adding player jumping

--- 3D FPS Project P4/Assets/Scripts/PlayerController.cs
+++ 3D FPS Project P4/Assets/Scripts/PlayerController.cs
@@ -6,8 +6,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 1f;
+    public float jumpForce = 10f;
+    public float gravityModifier = 1f;
     public float mouseSensitivity = 1f;
     public Transform theCamera;
+    public Transform groundCheckpoint;
+    public LayerMask whatIsGround;
+    private bool _canPlayerJump;
     private Vector3 _moveInput;
     private CharacterController _characterController;
 
@@ -20,6 +25,9 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Store the y velocity
+        float yVeclocity = _moveInput.y;
+
         //Player movement
         //_moveInput.x = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
         //_moveInput.z = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
@@ -29,13 +37,31 @@ public class PlayerController : MonoBehaviour
         _moveInput =(forwardDirection + horizontalDirection).normalized;
         _moveInput *= moveSpeed;
 
+        //Player jumping setup
+        _moveInput.y = yVeclocity;
+        _moveInput.y += Physics.gravity.y * gravityModifier * Time.deltaTime;
+
+        if(_characterController.isGrounded)
+        {
+            _moveInput.y = Physics.gravity.y * gravityModifier * Time.deltaTime;
+        }
+
+        //Checking to see if player can jump
+        _canPlayerJump = Physics.OverlapSphere(groundCheckpoint.position, 0.50f, whatIsGround).Length > 0;
+
+        //Apply a jump force to player
+        if(Input.GetKeyDown(KeyCode.Space) && _canPlayerJump)
+        {
+            _moveInput.y = jumpForce;
+        }
+
         _characterController.Move(_moveInput * Time.deltaTime);
 
         //Control camera rotation
         Vector2 mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mouseSensitivity;
 
         //Player Rotation
-        Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + mouseInput.x, transform.rotation.eulerAngles.z);
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + mouseInput.x, transform.rotation.eulerAngles.z);
 
         //Camera Rotation
         theCamera.rotation = Quaternion.Euler(theCamera.rotation.eulerAngles + new Vector3(-mouseInput.y, 0f, 0f));
